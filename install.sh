779fe88b78743e15018258f41429173bf8981504
Daniel Rozario <61970262+roza-rio@users.noreply.github.com>
2021-03-22 10:14:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@ sudo mv dotnet-core/* /opt/dotnet
 sudo mkdir /opt/openbullet2
 sudo chmod 777 /opt/openbullet2
 sudo mv OpenBullet2/* /opt/openbullet2
-wget https://raw.githubusercontent.com/roza-rio/openbullet2-installer/master/dotnet
 sudo mv dotnet /bin/
 sudo chmod +x /bin/dotnet
-wget https://raw.githubusercontent.com/roza-rio/openbullet2-installer/master/openbullet
 sudo mv openbullet /bin/
 sudo chmod +x /bin/openbullet
 echo "Done!"
